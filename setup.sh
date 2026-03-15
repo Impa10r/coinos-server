7c829c92243d80209113f3c0468e9fe2239d92af
Impa10r <101550606+Impa10r@users.noreply.github.com>
2026-03-15 22:50:12 +0100

    use nostr url from config

--- setup.sh
+++ setup.sh
@@ -2,6 +2,17 @@ cp config.ts.sample config.ts
 cp compose.yml.sample compose.yml
 cp -r sampledata data
 sudo chown 100:100 data/nostr/data
+
+# Create the data directory
+mkdir -p ./data/tigerbeetle
+
+# Initialize the data file
+docker run --rm \
+  --privileged \
+  -v $(pwd)/data/tigerbeetle:/data \
+  ghcr.io/tigerbeetle/tigerbeetle:latest \
+  format --cluster=0 --replica=0 --replica-count=1 /data/0_0.tigerbeetle
+
 docker compose up -d
 docker run -it -v $(pwd):/home/bun/app --entrypoint bun asoltys/coinos-server i
 docker exec -it bc bitcoin-cli createwallet coinos
