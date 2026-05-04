d636bcc49547f54a255247860fb875c209bec8fb
Impalor <101550606+Impa10r@users.noreply.github.com>
2026-05-04 22:57:09 +0000

    Add secret parameter to transaction confirmation request

--- sampledata/bitcoin/transaction.sh
+++ sampledata/bitcoin/transaction.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for i in 1 2 3 4 5; do
-  wget -q -O- --post-data='{"txid": "'$1'", "wallet": "'$2'", "type": "bitcoin"}' --header='Content-Type:application/json' 'http://app:3119/confirm' && exit 0
+  wget -q -O- --post-data='{"txid": "'$1'", "wallet": "'$2'", "type": "bitcoin", "secret": "YOUR_ADMINPASS"}' --header='Content-Type:application/json' 'http://app:3119/confirm' && exit 0
   sleep $i
 done
 echo "confirm failed for $1 after retries"
